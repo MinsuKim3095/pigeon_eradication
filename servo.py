0e5aaa79ccc68d9dda4a8b9f14bdc12268b4f150
MinsuKim3095 <68323158+MinsuKim3095@users.noreply.github.com>
2021-02-20 23:04:25 +0900

    210220A_serov.py
    210220A

--- servo.py
+++ servo.py
@@ -13,31 +13,67 @@ servo1 = GPIO.PWM(12,50)
 # Start PWM running on 2 servos, value of 0 (pulse off)
 servo1.start(0)
 
-# Turn servo1 
+# Read Coordinates
+# f = open('/home/minsu/coordinates.txt', 'r')
+# c = f.read()
+# f.close()
+# webcam_X_Center = 208
+# webcam_X_Center_Left = -30
+# webcam_X_Center_Right = 30
+# c = 208(webcam's x_center coordinates) - (object's x_center coordinates)
+# Turn servo1
+
 
 try :
-	while True:
-		servo1.ChangeDutyCycle(12.5)
-		time.sleep(2)
-		servo1.ChangeDutyCycle(10.0)
-		time.sleep(2)
-		servo1.ChangeDutyCycle(7.5)
-		time.sleep(2)
-		servo1.ChangeDutyCycle(5.0)
-		time.sleep(2)
-		servo1.ChangeDutyCycle(2.5)
-		time.sleep(2)
-		servo1.ChangeDutyCycle(5.0)
-		time.sleep(2)
-		servo1.ChangeDutyCycle(7.5)
-		time.sleep(2)
-		servo1.ChangeDutyCycle(10.0)
-		time.sleep(2)
+	while(1):
+		f = open('/home/minsu/coordinates.txt', 'r')
+		c = f.read()
+#		c = float()
+		f.close()
+		if c == None :
+			for i in range (0,180):
+				global DC
+				DC=1/18.*(i)+2
+				servo1.ChangeDutyCycle(DC)
+				time.sleep(.02)
+			for i in range(180,0,-1):
+				DC=1/18.*(i)+2
+				servo1.ChangeDutyCycle(DC)
+				time.sleep(.02)
+		else :
+			c = int(c)
+			if c < -30 :
+				DC -=1
+				servo1.ChangeDutyCycle(DC)
+				time.sleep(.02)
+			elif -30 < c < 30 :
+				servo1.stop()
+			else :
+				DC +=1
+				servo1.ChangeDutyCycle(DC)
+				time.sleep(.02)
+#try :
+	#while True:
+		#servo1.ChangeDutyCycle(12.5)
+		#time.sleep(2)
+		#servo1.ChangeDutyCycle(10.0)
+		#time.sleep(2)
+		#servo1.ChangeDutyCycle(7.5)
+		#time.sleep(2)
+		#servo1.ChangeDutyCycle(5.0)
+		#time.sleep(2)
+		#servo1.ChangeDutyCycle(2.5)
+		#time.sleep(2)
+		#servo1.ChangeDutyCycle(5.0)
+		#time.sleep(2)
+		#servo1.ChangeDutyCycle(7.5)
+		#time.sleep(2)
+		#servo1.ChangeDutyCycle(10.0)
+		#time.sleep(2)
 
 
 except KeyboardInterrupt:
 	servo1.stop()
-
 
 GPIO.cleanup()
 
